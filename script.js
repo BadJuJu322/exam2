0ed48b07720a075e4d5a25cb1a9fae3f626b149c
BadJuJu322 <157844350+BadJuJu322@users.noreply.github.com>
2025-03-28 19:31:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function loadProducts() {
   })
   .then(data => {
     console.log('Данные от сервера:', data);
-    localStorage.setItem('allProducts', JSON.stringify(data));
+    //localStorage.setItem('allProducts', JSON.stringify(data));
     displayCategories();
     applyFilters();
   })
