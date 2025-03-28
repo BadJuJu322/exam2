3e9a6325e6336e565b3453133155e3f0208de01a
BadJuJu322 <157844350+BadJuJu322@users.noreply.github.com>
2025-03-28 18:29:00 +0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -24,7 +24,7 @@ function formatDate(date) {
   const day = String(d.getDate()).padStart(2, '0')
   const month = String(d.getMonth() + 1).padStart(2, '0')
   const year = d.getFullYear()
-  return `${year}.${month}.${day}`
+  return `${day}.${month}.${year}`
 }
 
 function getGoods(page = 1, perPage = 10, query = '') {
